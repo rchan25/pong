55321900f94809730d551dcb10ff097b5e862206
rchan25 <rociochanp@gmail.com>
2022-11-04 16:12:09 -0500

    Movimiento de raquetas

--- entidades.py
+++ entidades.py
@@ -1,3 +1,4 @@
+from errno import ESOCKTNOSUPPORT
 import pygame as pg
 
 class Bola:
@@ -27,6 +28,16 @@ class Raqueta:
     def dibujar(self, pantalla):
         pg.draw.rect(pantalla, self.color, (self.center_x - self.w//2, self.center_y - self.h//2, self.w, self.h))
 
+    def mover(self, tecla_arriba, tecla_abajo, y_max=600):
+        estado_teclas = pg.key.get_pressed()
+        if estado_teclas[tecla_arriba]:
+            self.center_y -= self.vy
+        if self.center_y < self.h//2:
+            self.center_y = self.h//2
 
+        if estado_teclas[tecla_abajo]:
+            self.center_y += self.vy
+        if self.center_y > y_max - self.h//2:
+            self.center_y = y_max - self.h//2
 
     
--- main.py
+++ main.py
@@ -5,16 +5,24 @@ pg.init()
 
 pantalla_principal = pg.display.set_mode((800, 600))
 pg.display.set_caption("Pong")
+cronometro = pg.time.Clock()
 
 game_over = False
 bola = Bola(400, 300, color=(255,255,255))
 raqueta1 = Raqueta(20,300,w=20,h=120, color=(255,255,255))
 raqueta2 = Raqueta(780,300,w=20,h=120, color=(255,255,255))
+raqueta2.vy = 5
+raqueta1.vy = 5
 
 while not game_over:
+    dt = cronometro.tick(60)
+    print(dt) 
     for evento in pg.event.get():
         if evento.type == pg.QUIT:
             game_over = True
+
+    raqueta2.mover(pg.K_UP, pg.K_DOWN)
+    raqueta1.mover(pg.K_a, pg.K_z)
 
     pantalla_principal.fill((0, 0, 0))
     bola.dibujar(pantalla_principal)
